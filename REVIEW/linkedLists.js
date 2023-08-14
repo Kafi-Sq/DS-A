41953a886d25c93e0f93241f68cbdd31f9e67da7
Hassen Hassen <xasansq@gmail.com>
2023-08-14 14:35:10 -0500

    Singly linked lists

--- REVIEW/linkedLists.js
+++ REVIEW/linkedLists.js
@@ -41,12 +41,51 @@ class LinkedList {
         this.head = newNode
         this.length++
     }
+
+    printList() {
+        const arr = []
+        let curr = this.head
+        while(curr) {
+            arr.push(curr.value)
+            curr = curr.next
+        }
+        return arr
+    }
+
+    insert(index, value) {
+        if(index >= this.length) return this.append(value)
+        const newNode = new Node(value)
+        const prevNode = this.traverseToIndex(index - 1)
+        const holdingPointer = prevNode.next
+
+        prevNode.next = newNode
+        newNode.next = holdingPointer
+        this.length++
+    }
+
+    remove(index) {
+        const prevNode = this.traverseToIndex(index - 1)
+        const holdingPointer = prevNode.next
+        prevNode.next = holdingPointer.next
+        this.length--
+    }
+
+    traverseToIndex(index) {
+        let count = 0
+        let curr = this.head
+        while(count !== index) {
+            curr = curr.next
+            count++
+        }
+        return curr
+    }
 }
 
 const myLinked = new LinkedList(5)
 myLinked.append(10)
 myLinked.append(16)
 myLinked.append(17)
-myLinked.prepend(3)
 myLinked.prepend(1)
-console.log(myLinked)
+myLinked.insert(2, 8)
+myLinked.remove(5)
+console.log(myLinked.printList())
